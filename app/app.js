e72d37d595aa200c6958585ab882a207bacbcc58
Nahiska <stefyluddeni@gmail.com>
2023-01-03 22:35:06 -0300

    Creación de rutas bases

---
+++ app/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.use("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.listen(PORT, () => console.log(`Server listen in port ${PORT}\n http://localhost:${PORT}`));
