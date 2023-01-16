d8f121fad3f461765690e0aa6be8920c4fdc2707
Nahiska <stefyluddeni@gmail.com>
2023-01-16 03:24:50 -0300

    product Detail

--- app/app.js
+++ app/app.js
@@ -9,4 +9,10 @@ app.use("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
+/* PRODUCT DETAIL */
+
+app.use("/productDetail", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productDetail.html"));
+});
+
 app.listen(PORT, () => console.log(`Server listen in port ${PORT}\n http://localhost:${PORT}`));
