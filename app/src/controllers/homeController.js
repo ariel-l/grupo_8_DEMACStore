62ac6ca316640e101da2cc7bc98d9042205caa00
ariel-l <ariel.latour@hotmail.com>
2023-02-20 14:42:41 -0300

    added list products view, route and controller

--- app/src/controllers/homeController.js
+++ app/src/controllers/homeController.js
@@ -1,6 +1,20 @@
+const { readJSON } = require('../database')
+
+const products = readJSON('products.json')
+const formatNumber = number => number.toLocaleString('es-AR', {maximumFractionDigits:0});
+
 module.exports = {
     index: (req, res) => {
-        return res.render('home')
+
+        const productsInSale = products.filter(product => product.discount > 0)
+        const productsRecommended = products.filter(product => product.category === 'refurbished')
+
+
+        return res.render('home', {
+            productsInSale,
+            productsRecommended,
+            formatNumber
+        })
     }
 }
 
