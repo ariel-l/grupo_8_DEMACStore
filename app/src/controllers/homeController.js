b32ae71c70e51e134e5badb5e41b7bef14877a70
ariel-l <ariel.latour@hotmail.com>
2023-02-22 02:01:11 -0300

    add shuffle to array fix

--- app/src/controllers/homeController.js
+++ app/src/controllers/homeController.js
@@ -6,14 +6,11 @@ const formatNumber = number => number.toLocaleString('es-AR', {maximumFractionDi
 function shuffle(array) {
     let currentIndex = array.length; let  randomIndex;
   
-    // While there remain elements to shuffle.
     while (currentIndex !== 0) {
   
-      // Pick a remaining element.
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
   
-      // And swap it with the current element.
       [array[currentIndex], array[randomIndex]] = [
         array[randomIndex], array[currentIndex]];
     }
