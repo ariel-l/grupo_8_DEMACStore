30c4736f59b308276b747baa6c1174fe1b1b0e97
estefania-luddeni <stefyluddeni@gmail.com>
2023-03-04 03:53:49 -0300

    start layout and route configuration userProfile

--- app/src/controllers/usersControllers.js
+++ app/src/controllers/usersControllers.js
@@ -1,8 +1,22 @@
+const { readJSON, writeJSON } = require('../database/index');
+
+const users = readJSON('users.json');
+
 module.exports = {
     login: (req, res) => {
         return res.render('users/login')
     },
     register: (req, res) => {
         return res.render('users/register')
+    },
+    profile: (req, res) => {
+
+        const userId = Number(req.params.id);
+
+        const user = users.find(user => user.id === userId);
+
+        res.render('users/userProfile'), {
+            user
+        }
     }
 }
--- app/src/routes/users.js
+++ app/src/routes/users.js
@@ -9,7 +9,8 @@ router.get('/login', controller.login);
 /* GET REGISTER PAGE */
 router.get('/register', controller.register);
 
-
+/* GET USER PROFILE PAGE */
+router.get('/profile', controller.profile);
 
 /* EXPORT ROUTER */
 module.exports = router;
