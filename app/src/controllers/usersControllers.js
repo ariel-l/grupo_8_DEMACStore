aa0fa70742f9d8eabbf8a7dbbc9c6985d8db566c
camila-yniguez <cami.yniguez@gmail.com>
2023-03-18 01:08:06 -0300

    Add userEditProfile ejs, routes and controller

--- app/src/controllers/usersControllers.js
+++ app/src/controllers/usersControllers.js
@@ -1,8 +1,55 @@
+const { readJSON, writeJSON } = require('../database/index');
+const users = readJSON('users.json');
+
 module.exports = {
     login: (req, res) => {
         return res.render('users/login')
     },
     register: (req, res) => {
         return res.render('users/register')
-    }
+    },
+    editProfile: (req, res) => {
+        let userInSessionId = req.session.user.id;
+
+        let userInSession = users.find(user => user.id === userInSessionId);
+
+        res.render('users/userEditProfile', {
+            user: userInSession,
+            session: req.session
+        })
+    },
+    updateProfile: (req, res) => {
+        let errors = validationResult(req);
+        if(errors.isEmty()){
+            let userId = req.session.user.id;
+            let user = users.find(user => user.id === userId);
+    
+            const {
+                username,
+                name,
+                lastName,
+                tel,
+                adress,
+                postal_code,
+                province,
+                city
+            } = req.body;
+    
+            user.username = username;
+            user.name = name;
+            user.lastName = lastName;
+            user.avatar = req.file ? req.file.filename : user.avatar;
+            user.tel = tel;
+            user.adress = adress;
+            user.postal_code = postal_code;
+            user.province = province;
+            user.city = city;
+        
+        }
+
+    
+        writeJSON('users.json', users)
+
+        res.redirect('/')
+    },
 }
--- app/src/routes/users.js
+++ app/src/routes/users.js
@@ -9,7 +9,11 @@ router.get('/login', controller.login);
 /* GET REGISTER PAGE */
 router.get('/register', controller.register);
 
+/* GET - USER PROFILE EDIT FORM */
+router.get('/profile/edit', userInSessionCheck, editProfile);
 
+/* PUT - USER PROFILE EDIT FORM */
+router.put('/profile/edit', updateProfile )
 
 /* EXPORT ROUTER */
 module.exports = router;
