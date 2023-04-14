4964c1ed2242ba3b24b26b26cfbaaa20e1e325cb
MelinaLabra <melina.lb04@gmail.com>
2023-04-13 22:37:56 -0300

    add models products

--- app/src/database/models/Product.js
+++ app/src/database/models/Product.js
@@ -1,84 +1,102 @@
 module.exports = (sequelize, dataTypes) => {
-    let alias = "Products";
+    let alias = "Product";
     let cols = {
         id: {
             type: dataTypes.INTEGER(11),
             allowNull: false,
             autoIncrement: true,
-            primaryKey: true
+            primaryKey: true,
         },
         name: {
             type: dataTypes.STRING(40),
-            allowNull: false
+            allowNull: false,
         },
         image: {
             type: dataTypes.STRING(100),
         },
-        brand: {
-            type: dataTypes.STRING(40)
+        brandID: {
+            type: dataTypes.STRING(40),
+            allowNull: false,
         },
         price: {
             type: dataTypes.INTEGER(11),
-            allowNull: false
+            allowNull: false,
         },
         discount: {
             type: dataTypes.INTEGER(11),
         },
         subcategoryID: {
             type: dataTypes.INTEGER(11),
-            allowNull: false
+            allowNull: false,
         },
         model: {
-            type: dataTypes.STRING(40)
+            type: dataTypes.STRING(40),
         },
         os: {
-            type: dataTypes.STRING(20)
+            type: dataTypes.STRING(20),
         },
         screen: {
-            type: dataTypes.STRING(20)
+            type: dataTypes.STRING(20),
         },
         internalMemory: {
-            type: dataTypes.STRING(20)
+            type: dataTypes.STRING(20),
         },
         ram: {
-            type: dataTypes.STRING(20)
+            type: dataTypes.STRING(20),
         },
         chipset: {
-            type: dataTypes.STRING(20)
+            type: dataTypes.STRING(20),
         },
         frontCamera: {
-            type: dataTypes.STRING(20)
+            type: dataTypes.STRING(20),
         },
         mainCamera: {
-            type: dataTypes.STRING(20)
+            type: dataTypes.STRING(20),
         },
         video: {
-            type: dataTypes.STRING(100)
+            type: dataTypes.STRING(100),
         },
         battery: {
-            type: dataTypes.STRING(45)
+            type: dataTypes.STRING(45),
         },
         dimensions: {
-            type: dataTypes.STRING(100)
+            type: dataTypes.STRING(100),
         },
         weight: {
-            type: dataTypes.STRING(20)
+            type: dataTypes.STRING(20),
         },
         cardSlot: {
-            type: dataTypes.STRING(20)
+            type: dataTypes.STRING(20),
         },
         description: {
-            type: dataTypes.STRING(450)
+            type: dataTypes.STRING(450),
+        },
+        createdAt: {
+            type: dataTypes.DATE,
+        },
+        updatedAt: {
+            type: dataTypes.DATE,
         }
     }
 
     let config = {
-        tableName: "products",
-        timestamps: true,
-        underscored: true
+           tableName: 'products',
+        // createdAt: "createdAt",
+        // updatedAt: "updatedAt",
     }
 
-    const Product = sequelize.define(alias, cols, config)
-    
+    const Product = sequelize.define(alias, cols, config);
+
+    Product.associate = function (models) {
+         Product.belongsTo(models.subcategoryID, {
+             as: "subcategories",
+             foreignKey: "subcategoryID"
+         })
+         Product.belongsTo(models.brandID, {
+             as: "brands",
+             foreignKey: "brandID"
+         })
+        }
+
     return Product;
 }
