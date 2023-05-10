d312b3f8bbd3ad56a0c437f93d67296ea2b755a4
camila-yniguez <cami.yniguez@gmail.com>
2023-05-10 20:12:01 -0300

    Add login front in process

---
+++ app/public/js/login.js
@@ -0,0 +1,84 @@
+let qy = (elemento) => {
+    return document.querySelector(elemento);
+};
+
+window.addEventListener("load", () => {
+    let $email = qy("#email"),
+        $emailErrors = qy("#emailErrors"),
+        $password = qy("#password"),
+        $passwordErrors = qy("#passwordErrors"),
+        $remember = qs('#remember'),
+        $rememberErrors = qs('#rememberErrors'),
+        $form = qy("#form"),
+        regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i, 
+    regExpassword = /^(?=.\d)(?=.[a-z])(?=.*[A-Z]).{6,12}$/;
+
+    $email.addEventListener("blur", () => {
+        switch (true) {
+                case !$email.value.trim():
+                    $emailErrors.innerText = "El email es obligatorio";
+                    $email.classList.add("is-invalid");
+                    break;
+                case !regExEmail.test($email.value):
+                    $emailErrors.innerText = "Por favor ingrese un email válido";
+                    $email.classList.add("is-invalid");
+                    break;
+                default:
+                    $email.classList.remove("is-invalid");
+                    $email.classList.add("is-valid");
+                    $emailErrors.innerText = "";
+                    break;
+        }
+    });
+
+    $password.addEventListener("blur", () => {
+        switch (true) {
+                case !$password.value.trim():
+                    $passwordErrors.innerText = "La contraseña es obligatoria";
+                    $password.classList.add("is-invalid");
+                    break;
+                case !regExpassword.test($password.value):
+                    $passwordErrors.innerText = "Email o contraseña inválido";
+                    $password.classList.add("is-invalid");
+                    break;
+                default:
+                    $password.classList.remove("is-invalid");
+                    $password.classList.add("is-valid");
+                    $passwordErrors.innerText = "";
+                    break;
+        }
+    });
+
+    $remember.addEventListener('click', () => {
+        $remember.value = 'on'
+        $remember.classList.toggle('is-valid');
+        $remember.classList.remove('is-invalid');
+        $rememberErrors.innerHTML = ""
+    });
+
+    $form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const FORM_ELEMENTS = event.target.elements;
+
+        for (let index = 0; index < FORM_ELEMENTS.length - 1; index++) {
+                const element = FORM_ELEMENTS[index];
+                if (element.value === "" && element.type !== "file") {
+                    element.classList.add("is-invalid");
+                }
+        }
+
+        let elementosConErrores = document.querySelectorAll(".is-invalid");
+        let errores = elementosConErrores.length > 0;
+
+        if (errores) {
+            submitErrors.innerText = "Hay errores en el formulario";
+        } else {
+            $form.submit();
+        }
+
+        if(!$remember.checked){
+            $remember.classList.add('is-invalid');
+            $rememberErrors.innerHTML = "Debes aceptar el recordatorio"
+        }
+    });
+});
